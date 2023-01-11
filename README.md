02832a37ee1fd118e5962de07add493c3a0262c6
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-11 15:01:05 -0600

    Step 1

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # Objective
 - Concurrency
+- Returning a package (or Future)
+- Async/Await is required to open a package to see what's inside Future
 
 # Requirements
-- [Live Server Extension]
+- [Live Server Extension] for parsing JSON
 - Use 10.0.2.2 instead of localhost for android emulator
+
+# Tip
+
+
+# Flow
+## Step 1. Future & Async, Await
+- Function returning Future 
+- - doesn't calculate its content inside the function itself
+- - Packages execution code to be called by the caller later in the future
+- - Only calculates the return (ex: Iterable<Person>)
+- - Other function should read its content
+- Function returning Async
+- - await on the result for whoever called this function
+- - and give the result back to that function
+- async is a way to mark your function as being able to use await syntax
+- await can wait on the result of Future in a function marked with async
+- Think parseJson function as pipeline
+- - 1st pipeline => Get URL with REQUEST Pipe
+- - 2nd pipeline => Parse json as string with Response Pipe
 
 # Resources
 - [Concurrency Tutorial](https://youtu.be/Rs9i8zJhN68)
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
@@ -17,11 +20,49 @@ void main() {
   );
 }
 
+@immutable
+class Person {
+  final String name;
+  final int age;
+
+  const Person({
+    required this.name,
+    required this.age,
+  });
+
+// Person Constructor: Instance of Person class with given json map
+  Person.fromJson(Map<String, dynamic> json)
+      : name = json['name'] as String,
+        age = json['age'] as int;
+
+  @override
+  String toString() => 'Person ($name, $age years old)';
+}
+
+const url = 'http://10.0.2.2:5500/api/people.json';
+
+Future<Iterable<Person>> parseJson() => HttpClient()
+    .getUrl(Uri.parse(url)) // 1st pipe to create request Future
+    .then((req) => req.close()) // Take req
+    .then((resp) => resp
+        .transform(utf8.decoder)
+        .join()) // Create response pipe & parse it as string
+    .then((str) =>
+        json.decode(str) as List<dynamic>) // Take string & parse it as json
+    .then((json) => json.map((e) => Person.fromJson(
+        e))); // json is split into various instances of Person class to become Future<Iterable> type
+
+void TestIt() async {
+  final persons = await parseJson();
+  persons.log();
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    TestIt();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
