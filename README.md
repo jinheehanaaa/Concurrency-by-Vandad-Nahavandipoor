a647009772959b0653cca0ba8e93aae6bfe3f56e
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-11 14:08:40 -0600

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Objective
+- Concurrency
+
+# Requirements
+- [Live Server Extension]
+- Use 10.0.2.2 instead of localhost for android emulator
+
+# Resources
+- [Concurrency Tutorial](https://youtu.be/Rs9i8zJhN68)
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+    );
+  }
+}
